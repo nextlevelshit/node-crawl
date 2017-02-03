8ca2728c137a6c5f0d65d68dedd418464fa0765e
Michael Czechowski <mail@dailysh.it>
2017-02-03 10:58:17 +0100

    socket running

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var express = require('express')
 var opn = require('opn')
 var app = express()
+var http = require('http').Server(app);
+var io = require('socket.io')(http)
+
+var SERVER_DOMAIN = 'localhost'
+var SERVER_PORT = 3000
+var DIST_DIR = `${__dirname}/dist`
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.sendFile(`${DIST_DIR}/index.html`)
 })
 
-app.listen(3000)
+io.on('connection', function(socket){
+  var THIS_CONSOLE_PREFIX = `[SOCKET.IO]\t`;
+  
+  console.log(`${THIS_CONSOLE_PREFIX} connection started`);
+  socket.on('button', function(param1, param2){
+    console.log(`${THIS_CONSOLE_PREFIX} button pressed: ${param1}, ${param2}`);
+  });
+});
+
+http.listen(SERVER_PORT, function(){
+  var THIS_CONSOLE_PREFIX = `[HTTP]\t\t`;
+  
+  console.log(`${THIS_CONSOLE_PREFIX} started web interface on http://${SERVER_DOMAIN}:${SERVER_PORT}`);
+  // open browser with web interface
+  console.log(`${THIS_CONSOLE_PREFIX} starting browser ...`)
+  opn(`http://${SERVER_DOMAIN}:${SERVER_PORT}`)
+});
